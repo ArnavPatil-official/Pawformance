bd06bda109404d98fa8ecf26277437a884dcca2f
Arnav <arnavrpatil0@gmail.com>
2025-03-29 11:07:11 -0400

    Initial Commit

---
+++ Pawformance/PawformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawformanceApp.swift
+//  Pawformance
+//
+//  Created by Arnav Patil on 3/29/25.
+//
+
+import SwiftUI
+
+@main
+struct PawformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawformanceTests/PawformanceTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawformanceTests.swift
+//  PawformanceTests
+//
+//  Created by Arnav Patil on 3/29/25.
+//
+
+import Testing
+@testable import Pawformance
+
+struct PawformanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
